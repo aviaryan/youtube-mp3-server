79497fc1f8141df6c3a1eb31e57e2924df669985
aviaryan <avi.aryan123@gmail.com>
2016-08-21 17:10:56 +0530

    test all playlists

--- tests/test_trending.py
+++ tests/test_trending.py
@@ -2,6 +2,7 @@ import unittest
 import json
 from tests import YMP3TestCase
 from ymp3.schedulers import trending
+from ymp3.helpers.data import trending_playlist
 
 
 class TestTrending(YMP3TestCase):
@@ -15,10 +16,11 @@ class TestTrending(YMP3TestCase):
         worker = scheduler.start()
         worker.join()
         # ^^ wait for above to finish
-        resp = self.app.get('/api/v1/trending')
-        self.assertEqual(resp.status_code, 200)
-        data = json.loads(resp.data)
-        self.assertNotEqual(data['metadata']['count'], 0)
+        for playlist in trending_playlist:
+            resp = self.app.get('/api/v1/trending?type=%s' % playlist[0])
+            self.assertEqual(resp.status_code, 200)
+            data = json.loads(resp.data)
+            self.assertNotEqual(data['metadata']['count'], 0)
 
 
 if __name__ == '__main__':
