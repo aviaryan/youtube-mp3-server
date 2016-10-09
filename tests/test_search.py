7a8a11b61aaa4f4da445249e231f251056d1de19
singhpratyush <singh.pratyush96@gmail.com>
2016-10-09 05:59:54 +0530

    Corrected tests.

--- tests/test_search.py
+++ tests/test_search.py
@@ -14,11 +14,11 @@ class TestSearch(YMP3TestCase):
         self.assertIn('Linkin Park', result)
         data = json.loads(result)
         self.assertEqual(len(data['results']), data['metadata']['count'])
-        self.assertTrue(len(data['results']) > 15, result)
+        self.assertTrue(len(data['results']) >= 15, result)
 
     def test_long_video_not_returned(self):
         """test search of a term which usually will have long videos as results"""
-        resp = self.app.get('/api/v1/search?q=codejam finals')
+        resp = self.app.get('/api/v1/search?q=kishore kumar')
         self.assertIn('Code Jam', resp.data)
         data = json.loads(resp.data)
         self.assertTrue(len(data['results']) < 10)
